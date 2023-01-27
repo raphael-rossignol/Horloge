c363234f235786b6e6d8f96ecfd8f57201ede793
raphael-rossignol <raphael.rossignol@laplateforme.io>
2023-01-27 14:14:34 +0100

    Commentary added

--- Horloge2.py
+++ Horloge2.py
@@ -1,6 +1,9 @@
 import time
 status = True
-list_time_alarm = (0, 0, 5)
+list_time_alarm = (0, 0, 5)     # Tuple for the alarm parameter
+
+
+# Function to print a message when the time is equal the alarm setting
 
 
 def alarm(time_alarm):
@@ -10,13 +13,16 @@ def alarm(time_alarm):
         pass
 
 
+# Function to print the time every second on the same line and have the other part of the alarm setting
+
+
 def change_time(new_hour):
     global list_time_alarm
     list_time_alarm = list(list_time_alarm)
     list_new_hour = list(new_hour)
     while status is True:
         list_new_hour[2] += 1
-        print("\r", list_new_hour[0], ":", list_new_hour[1], ":", list_new_hour[2], end="")
+        print("\r", list_new_hour[0], ":", list_new_hour[1], ":", list_new_hour[2], end="")  #Remove the former line for clarity
         if list_new_hour[2] >= 59:
             list_new_hour[1] += 1
             list_new_hour[2] = 0
@@ -25,12 +31,15 @@ def change_time(new_hour):
             list_new_hour[1] = 0
         if list_new_hour[0] >= 24:
             list_new_hour[0] = 0
-        if list_new_hour == list_time_alarm:
+        if list_new_hour == list_time_alarm:        # Starts the alarm function if the two list match
             alarm(list_time_alarm)
         time.sleep(1)
 
 
 change_time((23, 59, 50))
+
+
+# Display the actual time if no other function is active
 
 
 def display_time():
